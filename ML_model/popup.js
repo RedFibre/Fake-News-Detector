9473e81fbbbd54381f9b68f9ad5346863440e60c
DevanshUpadhyay26 <Devanshupadhyay26@gmail.com>
2023-05-01 15:34:30 +0530

    few changes

--- ML_model/popup.js
+++ ML_model/popup.js
@@ -1,20 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var submitButton = document.getElementById('submit-button');
-  submitButton.addEventListener('click', function() {
-    var text = document.getElementById('news-text').value;
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:5000/predict');
-    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-    xhr.onload = function() {
-      var response = JSON.parse(xhr.responseText);
-      var prediction = response.prediction;
-      var predictionElement = document.getElementById('prediction-result');
-      if (prediction == 0) {
-        predictionElement.textContent = 'The news is Real';
-      } else {
-        predictionElement.textContent = 'The news is Fake';
-      }
-    };
-    xhr.send(JSON.stringify({text: text}));
-  });
+// Get the form element from the webpage
+const form = document.getElementById('news-form');
+console.log(form);
+// Listen for form submit event
+form.addEventListener('submit', function(event) {
+  // Prevent the default form submission behavior
+  event.preventDefault();
+  // Get the input text from the form
+  const input = document.getElementById('news-text').value;
+  // Send an AJAX request to the Flask server to get the prediction
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', 'http://localhost:5000/predict');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      // Update the webpage's DOM to display the prediction result
+      const prediction = JSON.parse(xhr.responseText).prediction;
+      const resultDiv = document.getElementById('prediction-result');
+      resultDiv.innerText = prediction === 0 ? 'The news is Real' : 'The news is Fake';
+    } else {
+      console.error(xhr.statusText);
+    }
+  };
+  xhr.onerror = function() {
+    console.error(xhr.statusText);
+  };
+  xhr.send(JSON.stringify({text: input}));
 });
